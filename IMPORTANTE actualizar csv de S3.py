4baaff5149f49dde81a6e9a68766befc7b3de31b
trevorseether <joseph.montoya@unmsm.edu.pe>
2025-09-08 16:15:29 -0500

    super actualización

---
+++ IMPORTANTE actualizar csv de S3.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  8 15:14:24 2025
+
+@author: Joseph Montoya
+"""
+# =============================================================================
+# ACTUALIZAR CSV EN S3, para actualizar tablas
+# =============================================================================
+
+import pandas as pd
+import boto3
+import json
+import io
+# import os
+# from datetime import datetime
+
+from pyathena import connect
+
+#%% Credenciales de AmazonAthena
+with open(r"C:/Users/Joseph Montoya/Desktop/credenciales actualizado.txt") as f:
+    creds = json.load(f)
+
+conn = connect(
+    aws_access_key_id     = creds["AccessKeyId"],
+    aws_secret_access_key = creds["SecretAccessKey"],
+    aws_session_token     = creds["SessionToken"],
+    s3_staging_dir        = creds["s3_staging_dir"],
+    region_name           = creds["region_name"]
+    
+    )
+
+#%% leer archivo de amazon
+query = '''select * from prod_datalake_master.ba__ejemplo1'''
+
+cursor = conn.cursor()
+cursor.execute(query)
+
+# Obtener los resultados
+resultados = cursor.fetchall()
+
+# Obtener los nombres de las columnas
+column_names = [desc[0] for desc in cursor.description]
+
+# Convertir los resultados a un DataFrame de pandas
+df = pd.DataFrame(resultados, columns=column_names)
+
+#%%
+df2 = pd.concat([df] * 2, ignore_index=True)  # duplicar
+
+#%%
+# Cliente de S3
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id        = creds["AccessKeyId"],
+    aws_secret_access_key    = creds["SecretAccessKey"],
+    aws_session_token        = creds["SessionToken"],
+    region_name              = creds["region_name"]
+)
+
+# ==== CONFIGURACIÓN ==== 
+bucket_name = "prod-datalake-raw-730335218320" 
+s3_prefix = "manual/ba/ejemplo1/" # carpeta lógica en el bucket 
+
+# ==== EXPORTAR A PARQUET EN MEMORIA ====
+csv_buffer = io.StringIO() 
+df2.to_csv(csv_buffer, index=False, encoding="utf-8-sig") 
+
+# Nombre de archivo con timestamp (opcional, para histórico) 
+s3_key = f"{s3_prefix}ejemplo1.csv" 
+
+# Subir directamente desde el buffer 
+s3.put_object(Bucket=bucket_name, 
+              Key=f"{s3_prefix}ejemplo1.csv", 
+              Body=csv_buffer.getvalue() 
+              )
+
+print(f"✅ Archivo subido a s3://{bucket_name}/{s3_key}")
+ 
+ 
+ 
