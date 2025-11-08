4d19466410ea6b777887fe8a0a1d8becc5ec3d91
trevorseether <joseph.montoya@unmsm.edu.pe>
2025-11-07 19:27:57 -0500

    actualización

--- CARGAS AMAZON ATHENA/IMPORTANTE generador de schema SANDBOX PARQUET.py
+++ CARGAS AMAZON ATHENA/IMPORTANTE generador de schema SANDBOX PARQUET.py
@@ -11,36 +11,126 @@ Created on Wed Sep 24 13:11:02 2025
 import pandas as pd
 # pip install pyarrow
 
-df = pd.read_excel('datos.xlsx')
+df = pd.read_csv('C:/Users/Joseph Montoya/Desktop/fac_monthly_portfolio/fac_monthly_portfolio.csv',
+                     dtype = {'Codigo_subasta': str,
+                              'Cierre_fecha': str,
+                              'codmes': str,
+                              'Producto'	: str ,
+                            'Ruc_cliente'	: str ,
+                            'Nombre_cliente'	: str ,
+                            'Ruc_proveedor'	: str ,
+                            'Nombre_proveedor'	: str ,
+                            'Fecha_desembolso'	: str ,
+                            'Moneda'	: str ,
+                            'Monto_neto_total_pendiente'	: float ,
+                            'Monto_financiado'	: float ,
+                            'Plazo'	: float ,
+                            'tasa_empresario'	: float ,
+                            'Monto_adelanto'	: float ,
+                            'Ejecutivo'	: str ,
+                            'Monto_total_pagado'	: float ,
+                            'Capital_pagado'	: float ,
+                            'Interes_pagado'	: float ,
+                            'Ultima_fecha_pago'	: str ,
+                            'Fecha_vencimiento'	: str ,
+                            'Q_desembolso'	: float ,
+                            'm_desembolso'	: float ,
+                            'Saldo_capital'	: float ,
+                            'Saldo_monto'	: float ,
+                            'status'	: str ,
+                            'Dias_atraso'	: float ,
+                            'm_desembolso_soles'	: float ,
+                            'Saldo_capital_soles'	: float ,
+                            'Monto_financiado_soles'	: float ,
+                            'Tipo_cambio'	: float ,
+                            'codmes_desembolso'	: int ,
+                            'PAR1_ms': float ,
+                            'PAR15_ms'	: float ,
+                            'PAR30_ms'	: float ,
+                            'PAR60_ms'	: float ,
+                            'PAR90_ms'	: float ,
+                            'PAR120_ms'	: float ,
+                            'PAR180_ms'	: float ,
+                            'PAR360_ms'	: float ,
+                            'FLAG_ORIGEN_OPERACION'	: str ,
+                            'new_clients'	: int ,
+                            'new_providers'	: int ,
+                            'Fuente'	: str ,
+                            'Comercial'	: str ,
+                            'tipificacion_operativa'	: str ,
+                            'confirmaron_todas_las_facturas_en_cavali_'	: str ,
+                            'correo_de_confirmacion'	: str ,
+                            'comision_estructuracion'	: float ,
+                            'comision_estructuracion_solarizado'	: float ,
+                            'tasa_all_in'	: float ,
+                            'utilidad_soles'	: float ,
+                            'fecha'	: str ,
+                            'day'	: int ,
+                            'month'	: int ,
+                            'year'	: int ,
+                            'day_name'	: str ,
+                            'week'	: int ,
+                            'quarter_name'	: str ,
+                            'semester'	: int ,
+                            'day_of_week'	: int ,
+                            'day_of_year'	: int ,
+                            'is_weekend'	: str ,
+                            'is_holiday'	: str ,
+                            'fiscal_year'	: int ,
+                            'fiscal_month'	: int ,
+                            'fiscal_quarter'	: str ,
+                            'year_month'	: str ,
+                            'Riesgo'	: str ,
+                            'financiado_soles_gestora'	: float ,
+                            'financiado_soles_fp'	: float ,
+                            'financiado_soles_crowd'	: float ,
+                            'saldo_gestora'	: float ,
+                            'saldo_fp'	: float ,
+                            'saldo_crowd': float ,
+                            'grupo_economico'	: str ,
+                            'actividad_economica'	: str ,
+                            })
+
+#%%
+df['Cierre_fecha'] = pd.to_datetime(df['Cierre_fecha']).dt.date
+df['Fecha_desembolso'] = pd.to_datetime(df['Fecha_desembolso']).dt.date
+df['Ultima_fecha_pago'] = pd.to_datetime(df['Ultima_fecha_pago']).dt.date
+df['Fecha_vencimiento'] = pd.to_datetime(df['Fecha_vencimiento']).dt.date
+df['fecha'] = pd.to_datetime(df['fecha']).dt.date
+
+#%%
+from datetime import datetime
+from zoneinfo import ZoneInfo
+################################################################################
+# Hora actual en Perú (UTC-5)
+now = datetime.now(ZoneInfo("America/Lima"))
+
+# Guardar directamente el objeto datetime
+df["_timestamp"] = now
 
 #%% 
 # pip install pyarrow
-df.to_parquet(r'C:/Users/Joseph Montoya/Desktop/ejemplo1/ejemplo1.parquet',
+df.to_parquet(r'C:/Users/Joseph Montoya/Desktop/fac_monthly_portfolio/fac_monthly_portfolio.parquet',
                  index = False,
                  )
 
 #%%
 
 # el nombre del csv, también debe ser el nombre de la carpeta
-nombre_parquet = 'ejemplo1'
+nombre_parquet = 'fac_monthly_portfolio'
 nombre_carpeta = nombre_parquet
 
-df = pd.read_parquet(r'C:/Users/Joseph Montoya/Desktop/ejemplo1/ejemplo1/ejemplo1.parquet')
+df = pd.read_parquet(r'C:/Users/Joseph Montoya/Desktop/fac_monthly_portfolio/fac_monthly_portfolio.csv')
 
-from datetime import datetime
-now = datetime.utcnow()
-athena_timestamp = now.strftime("%Y-%m-%d %H:%M:%S.000")
-
-df['_timestamp'] = athena_timestamp
-# df['_timestamp'] = pd.to_datetime(df['_timestamp'])
-df['_timestamp'] = df['_timestamp'].astype(str)
-
+#%%
 mapping_types = {
     'object'          : 'string',
     'float64'         : 'double',
     'int64'           : 'integer',
+    'int32'           : 'integer',
     'bool'            : 'boolean',
-    'datetime64[ns]'  : 'date'
+    'datetime64[ns]'  : 'date',
+    'datetime64[us, America/Lima]' : 'TIMESTAMP'
 }
 
 temp_schema = ""
